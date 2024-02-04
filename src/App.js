61c4eb664f947290ad51711607e096b7fd746083
Vikas Tiwari <30732434+ervikasti@users.noreply.github.com>
2024-02-05 01:55:39 +0530

    Feature created

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Modal from './components/Modal/Modal';
+import { useState } from 'react';
+
+function App() {
+
+  const [tooglemodal,setToogleModal] = useState(false);
+
+  const handleClick =()=>{
+    setToogleModal(!tooglemodal);
+  }
+
+  return (
+   <div>
+       <h1>User Details Modal</h1>
+       <button  onClick={handleClick}>Open Form</button>
+       {tooglemodal && <Modal toggle={handleClick}/>}
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react"
+
+const Modal = ({toggle}) => {
+
+    const styles = {
+
+        overlay:{
+            display:'flex',
+            'justify-content':'center',
+            'align-item':'center',
+            width:'100%',
+            height:'100%',
+            position:'fixed',
+            top:'0',
+            bottom:'0',
+            right:'0',
+            'background-color': 'rgb(152, 152, 152)',
+        },
+        modal:{
+            position:'absolute',
+            top:'20%',
+            left:'25%',
+            'min-height':'50%',
+            'min-width':'50%',
+            'align-items':'center',
+            display:'flex',
+            'flex-direction':'column',
+            'background-color': 'white',
+            color:'black',
+        },
+        form:{
+            display:'flex',
+            'flex-direction':'column',
+            'align-items':'center',
+        },
+
+    }
+    const [formdata,setFormData] = useState({
+        username:'',
+        email:'',
+        phone:'',
+        dob:'',
+    })
+    const inputHandler = (e) => {
+        setFormData({...formdata,[e.target.name]:e.target.value});
+    }
+    const handleForm = (e) => {
+        e.preventDefault();
+
+        let date = new Date(formdata.dob);
+        let today = new Date();
+        today.setHours(0,0,0,0);
+
+        if(formdata.phone.length<10){
+            alert('Invalid phone number. Please enter a 10-digit phone number.');
+        }else if(date >today){ 
+            alert('Invalid date of birth. Date of birth cannot be in the future.')
+            
+
+        }else{
+        setFormData({
+                username:'',
+                email:'',
+                phone:'',
+                dob:'',
+            });
+        }
+        
+       
+    }
+
+    return(
+        
+        <div className="modal">
+            <div onClick={toggle} style={styles.overlay}></div>
+            <div className="modal-content" style={styles.modal}>
+                <h2>Fill Details</h2>
+                <form style={styles.form} onSubmit={handleForm}>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" onChange={inputHandler} value={formdata.username}required/>
+
+                    <label htmlFor="email">Email Address:</label>
+                    <input type="email" id="email" name="email" onChange={inputHandler} value={formdata.email}required/>
+
+                    <label htmlFor="phone">Phone Number:</label>
+                    <input type="tel" id="phone" name="phone" onChange={inputHandler} value={formdata.phone}required/>
+
+                    <label htmlFor="dob">Date of Birth:</label>
+                    <input type="date" id="dob" name="dob" onChange={inputHandler} value={formdata.dob} required/>
+
+                    <button type="submit">Submit</button>
+                </form>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Modal;
