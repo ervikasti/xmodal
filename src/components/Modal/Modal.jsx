e4f8a109aee1aa5ee84c7565d100a6c0e5ada81d
Vikas Tiwari <30732434+ervikasti@users.noreply.github.com>
2024-02-05 12:09:34 +0530

    Fix: classname

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -76,7 +76,7 @@ const Modal = ({toggle}) => {
     }
 
     return(
-        <div style={styles.container}>
+        <div className="modal">
             <div className="modal" onClick={toggle} style={styles.overlay}></div>
             <div className="modal-content" style={styles.modal}>
                 <h2>Fill Details</h2>
